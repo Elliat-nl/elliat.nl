de0ae6c014dba6b9e01272c21fb4b98acc546ee8
Martin Middel <martin@elliat.nl>
2025-11-02 13:12:11 +0100

    Progress

--- public/presentations/2025/presentation.ts
+++ public/presentations/2025/presentation.ts
@@ -8,17 +8,53 @@ import "reveal.js/plugin/highlight/monokai.css";
 import "@picocss/pico";
 
 import prettierPluginXQuery from "prettier-plugin-xquery";
-import prettier from "prettier";
+import prettierPluginXml from "@prettier/plugin-xml";
+import prettier, { Plugin } from "prettier";
 const deck = new Reveal({
   plugins: [RevealHighlight, RevealNotes],
   controlsTutorial: false,
   history: true,
+  navigationMode: "linear",
 });
 
 await deck.initialize();
 
 const highlight = deck.getPlugin("highlight");
 
+const xsltplugin: Plugin = {
+  ...prettierPluginXml,
+  printers: {
+    xml: {
+      embed: (path, options) => {
+        const node = path.node;
+        if (node.name === "attribute") {
+          console.log("!!!");
+          return async (textToDoc, print) => {
+            const docNode = await textToDoc(node.content, {
+              parser: prettierPluginXQuery.parsers!.xquery!,
+            });
+
+            return group(docNode);
+          };
+        }
+        return prettierPluginXml.printer.embed(path, options);
+      },
+      ...prettierPluginXml,
+    },
+  },
+};
+
+const formatCodeBlock = async (cb: HTMLElement, language: string) => {
+  const formatted = await prettier.format(cb.textContent ?? "", {
+    plugins: [prettierPluginXQuery, xsltplugin],
+    parser: language,
+  });
+
+  console.log(formatted);
+  cb.replaceChildren(document.createTextNode(formatted));
+  delete cb.dataset.highlighted;
+  highlight.highlightBlock(cb);
+};
 class PrettifyButton extends HTMLButtonElement {
   private _clickHandler: () => void;
   constructor() {
@@ -29,15 +65,16 @@ class PrettifyButton extends HTMLButtonElement {
           [],
       ) as HTMLElement[];
       for (const cb of xqueryCodeBlocks) {
-        const formatted = await prettier.format(cb.textContent ?? "", {
-          plugins: [prettierPluginXQuery],
-          parser: "xquery",
-        });
-
-        console.log(formatted);
-        cb.replaceChildren(document.createTextNode(formatted));
-        delete cb.dataset.highlighted;
-        highlight.highlightBlock(cb);
+        const language = cb.hasAttribute("data-xquery-4")
+          ? "xquery4"
+          : "xquery";
+        await formatCodeBlock(cb, language);
+      }
+      const xsltCodeBlocks = Array.from(
+        this.closest("section")?.querySelectorAll(".language-xslt code") ?? [],
+      ) as HTMLElement[];
+      for (const cb of xsltCodeBlocks) {
+        await formatCodeBlock(cb, "xml");
       }
     };
   }
