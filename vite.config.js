0306adb6a525b63542f74e7e9b0505ff339d8fd6
Martin Middel <martin@elliat.nl>
2026-02-04 09:10:49 +0100

    host presentations

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+import { lstatSync, readdirSync } from "node:fs";
 import { readdir } from "node:fs/promises";
-import { dirname, resolve } from "node:path";
+import { dirname, relative, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
@@ -11,20 +12,37 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
  * @returns {Record<string, string>}
  */
 function accumulateFiles(pathByName, entry) {
+  // Might be a dir
+  if (lstatSync(entry).isDirectory()) {
+    return readdirSync(entry)
+      .filter((entry) => entry !== "node_modules")
+      .map((subentry) => resolve(entry, subentry))
+      .reduce(accumulateFiles, pathByName);
+  }
+
   if (!entry.endsWith(".html")) {
+    // Only host html files
     return pathByName;
   }
 
-  return { ...pathByName, [entry]: resolve(__dirname, "public", entry) };
+  return {
+    ...pathByName,
+    [relative(resolve(__dirname, "public"), entry)]: resolve(entry),
+  };
 }
 
 export default defineConfig(async () => {
   const files = await readdir(resolve(__dirname, "public"));
-  const pathByName = files.reduce(accumulateFiles, {
-    main: resolve(__dirname, "public", "index.html"),
-  });
+
+  const pathByName = files
+    .map((file) => resolve(__dirname, "public", file))
+    .reduce(accumulateFiles, {
+      404: resolve(__dirname, "public", "404.html"),
+      main: resolve(__dirname, "public", "index.html"),
+    });
 
   console.log(pathByName);
+
   return {
     root: "./public",
     publicDir: false,
